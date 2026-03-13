b7f62002a59ebd097a1f74af77c9e277e9483f72
Katelyn <katelynyewwork@gmail.com>
2026-03-13 14:52:51 -0400

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-cloned the alley website using js mostly, created static html elements and other things for soley practicing JS skills.
--- src/aboutScroll.js
+++
@@ -1,8 +0,0 @@
-function setUpAboutScroll() {
-    const about = document.querySelector("#about");
-    window.addEventListener("scroll", () => {
-        const scrollY = window.scrollY;
-        about.style.transform = `translateY(${scrollY * 0.2}px)`;
-    });
-}
-export default setUpAboutScroll;
